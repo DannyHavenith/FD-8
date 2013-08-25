f60322432031e07272c1e18fb2dfd603fd84ef01
Danny <dh.programming@havenith.net>
2013-08-25 12:14:29 +0200

    First version with working pot SPI and implementation of ADC

---
+++ fd8.cpp
@@ -0,0 +1,119 @@
+/*
+ * fd8.cpp
+ *
+ *  Created on: Aug 11, 2013
+ *      Author: danny
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+#include "avr_utilities/devices/bitbanged_spi.h"
+
+/// This struct defines which pins are used by the bit banging spi device
+struct spi_pins {
+	DEFINE_PIN( mosi, B, 0);
+	DEFINE_PIN( miso, B, 1);
+	DEFINE_PIN( clk,  B, 2);
+};
+DEFINE_PIN( select_potmeter, B, 3);
+DEFINE_PIN( led, B, 1);
+
+/// template meta function that, given a cpu frequency and a required maximum frequency, finds a divider (as a power of two) that
+/// will result in the highest result frequency that is at most the given result frequency.
+template<uint16_t cpu_khz, uint16_t max_khz, uint8_t proposed = 0, bool result_fits = cpu_khz/(1<<proposed) <= max_khz>
+struct divider{};
+
+template<uint16_t cpu_khz, uint16_t max_khz, uint8_t proposed>
+struct divider<cpu_khz, max_khz, proposed, true>
+{
+	static const uint8_t value = proposed;
+};
+
+template<uint16_t cpu_khz, uint16_t max_khz, uint8_t proposed>
+struct divider<cpu_khz, max_khz, proposed, false>
+{
+	// the proposed divider still resulted in a too high frequency,
+	// try a higher divider (power of two).
+	static const uint8_t value = divider<cpu_khz, max_khz, (proposed + 1)>::value;
+};
+
+template<uint16_t value>
+struct dummy {};
+
+namespace
+{
+	typedef bitbanged_spi<spi_pins> spi;
+	void write_pot( uint8_t value)
+	{
+		reset( select_potmeter);
+		spi::transmit( 0b0001001100000000 | (uint16_t)value);
+		set( select_potmeter);
+	}
+
+
+	// bunch of adc functions. To be added to adc class
+
+	/// Tell the ADC component to start measurement.
+	void start_adc()
+	{
+		// set ADSC in ADCSRA
+		ADCSRA |= _BV( ADSC);
+	}
+
+	void init_adc(uint8_t channel)
+	{
+		// given the clock frequency, determine which power of 2 we need to bring it down to
+		// 200kHz.
+		static const uint8_t div =  divider< F_CPU/1000, 200>::value;
+
+		//set ADMUX, channel, REFS0 = Vcc (0) ADLAR = right adjust (0)
+		ADMUX = (channel & 0x03) << MUX0;
+
+		// set ADC Enable, ADATE none (0), clock div factor
+		ADCSRA = _BV( ADEN) | (div & 0x03);
+
+		ADCSRB = 0;
+
+	}
+
+	void wait_for_adc_result()
+	{
+		// wait--spinlock--for the ADSC bit to become zero again.
+		while( (ADCSRA & _BV(ADSC)));
+	}
+
+	uint16_t read_adc()
+	{
+		// read ADCL and ADCH in the right order.
+		uint16_t result=0;
+		result = ADCL;
+		result |= (static_cast<uint16_t>( ADCH) << 8);
+		return result;
+	}
+
+	uint8_t read_pedal()
+	{
+		start_adc();
+		wait_for_adc_result();
+
+		return read_adc() >> 2;
+	}
+} // unnamed namespace
+
+int main()
+{
+
+	set( select_potmeter);
+	spi::init();
+	init_adc( 2);
+	make_output( led | select_potmeter);
+	for(;;)
+	{
+		reset( led);
+		write_pot( 1);
+		_delay_ms(1000);
+
+		set(led);
+		write_pot(254);
+		_delay_ms(1000);
+	}
+}
