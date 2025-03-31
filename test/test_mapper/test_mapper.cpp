33f91601643ddfc64f0e85dd2523c931c2e32e50
Enrico Gueli <enrico.gueli@gmail.com>
2025-03-31 22:58:54 +0200

    basic (no-op) mapper test

--- test/test_mapper/test_mapper.cpp
+++ test/test_mapper/test_mapper.cpp
@@ -1,24 +1,24 @@
 #include <unity.h>
+#include "adc.h"
+#include "mapper.h"
+
+adc adc;
+PedalMapper mapper;
 
 void setUp(void) {
-    // set stuff up here
+    mapper.init_pedal_calibration(adc);
 }
 
 void tearDown(void) {
     // clean stuff up here
 }
 
-void test_foo(void) {
-    TEST_ASSERT_EQUAL(2, 2);
-}
-
-void test_bar(void) {
-    TEST_ASSERT_EQUAL(2, 3);
+void test_mapper(void) {
+    mapper.read_scaled_pedal(adc);
 }
 
 int main( int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_foo);
-    RUN_TEST(test_bar);
+    RUN_TEST(test_mapper);
     UNITY_END();
 }
