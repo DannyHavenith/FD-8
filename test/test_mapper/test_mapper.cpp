6d088bd04d22929b49ec9a62766c8c08552ff39e
Enrico Gueli <enrico.gueli@gmail.com>
2025-04-02 13:00:22 +0200

    test mapper calibration (failing)

--- test/test_mapper/test_mapper.cpp
+++ test/test_mapper/test_mapper.cpp
@@ -6,7 +6,6 @@ adc adc;
 PedalMapper mapper;
 
 void setUp(void) {
-    mapper.init_pedal_calibration(adc);
 }
 
 void tearDown(void) {
@@ -14,7 +13,19 @@ void tearDown(void) {
 }
 
 void test_mapper(void) {
+    // start at some rest value to perform initial calibration
+    adc.test_set(508);
+    mapper.init_pedal_calibration(adc);
+
+    // simulate a full push down to do self-calibration
+    adc.test_set(463);
     mapper.read_scaled_pedal(adc);
+    // the mapper is now calibrated and ready for business.
+
+    // At rest, the pot value should be near 255 (not quite 255 because of
+    // rounding errors)
+    adc.test_set(508);
+    TEST_ASSERT_GREATER_OR_EQUAL_CHAR(250, mapper.read_scaled_pedal(adc));
 }
 
 int main( int argc, char **argv) {
